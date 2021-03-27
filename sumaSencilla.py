b04ada21785b70beec29731f4cd9a6eccfc53761
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-03-27 14:24:32 -0500

    1er ejercicio clase 27/03

---
+++ sumaSencilla.py
@@ -0,0 +1,16 @@
+
+# Array del las Sumas 
+resultados = []
+
+def suma ():
+ x = int(input("Ingrese un Número:  "))
+ y = int(input("Ingrese un Número:  "))
+
+ return x + y
+
+for i in range(3):
+ resultados[suma[]].append(suma())
+ print("El resultado de su suma es:  ", resultados[i])
+
+print("El resultado de todas las sumas es:  ", resultados)
+
