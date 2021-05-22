bd2577ed467b19e30c007a40f57571bec4e4cab7
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-05-22 14:39:38 -0500

    Agregado Ejercicio 2

--- facil_2.py
+++ facil_2.py
@@ -6,3 +6,46 @@
    25%, si es azul un 50% y si es roja un 100%. Determinar la cantidad final que el
    cliente deberá pagar por su compra. se sabe que solo hay bolitas de los colores mencionados
 """
+
+from random import randint
+
+class compra:
+   def __init__ (self, valorCompra, colorBola):
+      self.valorCompra = valorCompra
+      self.colorBola = colorBola
+
+   def info (self):
+      return [self.valorCompra, self.colorBola]
+
+infoCompras = []
+
+valorBolas = [
+   0,
+   0.1,
+   0.25,
+   0.5,
+   1
+]
+colorBolas = [
+   "Blanco",
+   "Verde",
+   "Amarillo",
+   "Azul",
+   "Rojo"
+]
+arrBolas = [valorBolas, colorBolas]
+
+
+for i in range(6):
+   infoCompras.append(compra(
+      float(input("")),
+      randint(0, 4)
+   ))
+
+   totalCompra = infoCompras[i].info()[0] - (infoCompras[i].info()[0] * arrBolas[0][infoCompras[i].info()[1]])
+
+   print("******************************************\n")
+   print("Su Compra es de:  ", infoCompras[i].info()[0])
+   print("El Color de la Bola es:  ", arrBolas[1][infoCompras[i].info()[1]])
+   print("Valor total de la Compra, con descuento, es de:   ", totalCompra)
+   print("\n******************************************")
