817b55fffc33d3b0aed61b7a0463701ca477261b
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-05-22 17:07:58 -0500

    Agregado 3er Ejercicio clase

--- facil_3.py
+++ facil_3.py
@@ -7,3 +7,24 @@
    más se les da un 40% de descuento. El precio de cada computadora es de
    $3.500.000
 """
+
+valorPc = 3500000
+
+for i in range(3):
+   numPc = int(input("Ingrese el Número de Computadores Comprados:  "))
+   valorCompra = numPc * valorPc
+
+   if numPc < 5:
+      descuento = valorCompra * 0.1
+
+   elif numPc >=5 and numPc < 10:
+      descuento = valorCompra * 0.2 
+      
+   else:
+      descuento = valorCompra * 0.4
+
+   totalCompra = valorCompra - descuento
+
+   print("*****************************\n")
+   print("El total de la compra es de:  ", totalCompra)
+   print("\n*****************************")
