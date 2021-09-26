e59afd05933b3bf230eac17bd0dffa1e55112169
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-09-26 17:36:29 -0500

    Agregado clase Label-Input, function show_data

--- formulario_tkinter-optimizado.py
+++ formulario_tkinter-optimizado.py
@@ -1,48 +1,44 @@
 from tkinter import *
 
-base = Tk()
-screen_height = int(base.winfo_screenheight() / 4)
-screen_width = int(base.winfo_screenwidth() / 4)
+class label_input ():
+  def __init__ (self, parent, labeltext="", typeinput="str"):
+    self.label = Label(parent, text=labeltext)
 
-base["bg"] = "#f5f5f5"
-base["width"] = screen_width
+    if typeinput == "str":
+      self.input = StringVar()
+    elif typeinput == "int":
+      self.input = IntVar()
+
+    self.entry = Entry(parent, textvariable=self.input)
+
+  def show_data (self, initial_row=0, initial_column=0, span=1):
+    self.label.grid(row=initial_row, column=initial_column, columnspan=span)
+    initial_column += span
+    self.entry.grid(row=initial_row, column=initial_column, columnspan=span)
 
 def datos_ingresados ():
-  nombre = input_name.get()
-  apellido = input_lastName.get()
-  edad = input_edad.get()
+  nombre = name.input.get()
+  apellido = lastName.input.get()
+  edad = age.input.get()
 
   datos = Label(base)
   datos["text"] = f"Hola. Bienvenido {nombre} {apellido}, tienes {edad} años"
 
   datos.grid(row=5, column=0, columnspan=3, sticky=W)
 
-# Inputs Values
-input_name = StringVar()
-input_lastName = StringVar()
-input_edad = IntVar()
+base = Tk()
 
-# Labels
 main_label = Label(base, text="Ingresa tus Datos:  ")
-label_name = Label(base, text="Nombre: ")
-label_lastName = Label(base, text="Apellido: ")
-label_edad = Label(base, text="Edad:")
-
-# Inputs Entries
-entry_name = Entry(base, width=50, textvariable=input_name)
-entry_lastName = Entry(base, width=50, textvariable=input_lastName)
-entry_edad = Entry(base, width=50, textvariable=input_edad)
-
+name = label_input(base, labeltext="Nombre: ")
+lastName = label_input(base, labeltext="Apellido: ")
+age = label_input(base, labeltext="Edad: ", typeinput="int")
 start_button = Button(base, width=50, text="Ingresar Datos", command=datos_ingresados)
 
 # Join / Show
 main_label.grid(row=0, column=0)
-label_name.grid(row=1, column=1)
-entry_name.grid(row=1, column=2)
-label_lastName.grid(row=2, column=1)
-entry_lastName.grid(row=2, column=2)
-label_edad.grid(row=3, column=1)
-entry_edad.grid(row=3, column=2)
+name.show_data(1, 1)
+lastName.show_data(2, 1)
+age.show_data(3, 1)
 start_button.grid(row=4, column=0, columnspan=3)
 
 base.mainloop()
